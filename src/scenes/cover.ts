041c6a60dda9580f2a6f7c3f9ec078fff219829b
Mark <mark@remarkablemark.org>
2026-08-16 13:56:30 -0400

    refactor(cover): fix text in mobile

--- src/scenes/cover.ts
+++ src/scenes/cover.ts
@@ -1,21 +1,38 @@
 import { GAME, SCENE } from '../constants'
 
+const LOGO_HEIGHT = 512 * 0.5
+const HALF_LOGO = LOGO_HEIGHT / 2
+
 scene(SCENE.COVER, () => {
+  const isMobile = width() < 450
+
   setBackground(...GAME.BACKGROUND_COLOR)
 
-  add([
-    sprite('logo'),
-    pos(center().sub(0, 60)),
+  const title = make([
+    text('Number Bumper', {
+      size: 56,
+      align: 'center',
+      width: width(),
+    }),
+    pos(),
     anchor('center'),
-    scale(0.5),
-    fixed(),
+    color(GAME.UI_TEXT_COLOR),
   ])
 
-  add([
-    text('Number Bumper', { size: 56, align: 'center' }),
-    pos(center().add(0, 100)),
+  const subtitle = make([
+    text(`Bump the number.${isMobile ? '\n' : ' '}Hit the goal.`, {
+      size: 30,
+      align: 'center',
+    }),
+    pos(),
     anchor('center'),
-    color(...GAME.UI_TEXT_COLOR),
-    fixed(),
+    color(GAME.UI_HINT_COLOR),
   ])
+
+  add(title)
+  add([sprite('logo'), pos(center()), anchor('center'), scale(0.5)])
+  add(subtitle)
+
+  title.pos = center().sub(0, HALF_LOGO + title.height / 2.5)
+  subtitle.pos = center().add(0, HALF_LOGO + subtitle.height / 2)
 })
